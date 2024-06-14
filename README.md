dea77de78cc4d0302aef93b5a3d422cfd64886c8
Rahhul1309 <abbavaram.r@northeastern.edu>
2024-06-14 02:07:29 -0400

    chore:added the code

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# DB-migration
+# Flyway DB Migration Repository
+
+This repository contains database migration scripts managed by Flyway. Flyway is an open-source database migration tool that helps to version control your database schema and apply changes consistently across different environments.
+
+## Features
+
+- Version control for database schema.
+- Consistent and reliable schema migrations.
+- Support for multiple database systems.
+- Integration with CI/CD pipelines.
+
+## Prerequisites
+
+- Flyway (CLI, Docker, or any other supported method)
+- PostgreSQL database (or any other supported database system)
+
+## Setup
+
+### Local Setup
+
+1. **Clone the repository:**
+
+   ```sh
+   git clone https://github.com/yourusername/flyway-db-migration.git
+   cd flyway-db-migration
+   docker pull flyway/flyway
+   flyway migrate
+   flyway undo
+
+
+
--- sql/V0__create_table.sql
+++ sql/V0__create_table.sql
@@ -1,12 +1,20 @@
-CREATE TABLE IF NOT EXISTS cve_records (
+-- 000001_create_cve_table.up.sql
+
+-- Create the cve schema if it doesn't exist
+CREATE SCHEMA IF NOT EXISTS cve;
+
+-- Create the cve table within the cve schema
+CREATE TABLE IF NOT EXISTS cve.cve (
     id SERIAL PRIMARY KEY,
     cve_id VARCHAR(50) NOT NULL,
+    version TIMESTAMP NOT NULL,
     json_data JSONB NOT NULL,
     date_published TIMESTAMP NOT NULL,
     date_updated TIMESTAMP NOT NULL,
-    UNIQUE(cve_id, date_updated)
+    UNIQUE(cve_id, version)
 );
 
-CREATE INDEX IF NOT EXISTS idx_cve_id ON cve_records (cve_id);
-
-CREATE INDEX IF NOT EXISTS idx_date_updated ON cve_records (date_updated);
+CREATE INDEX IF NOT EXISTS idx_cve_id ON cve.cve(cve_id);
+CREATE INDEX IF NOT EXISTS idx_date_updated ON cve.cve(date_updated);
+CREATE INDEX IF NOT EXISTS idx_version ON cve.cve(version);
+CREATE INDEX IF NOT EXISTS idx_cve_json_data_gin ON cve.cve USING GIN (json_data);
